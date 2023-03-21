b040204836f402201387e1fa74197b00f145e966
Carson R <carsonaurum1@icloud.com>
2023-03-20 19:39:31 -0500

    Initial Commit

---
+++ DataSim/DataSimApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataSimApp.swift
+//  DataSim
+//
+//  Created by Carson Rau on 3/20/23.
+//
+
+import SwiftUI
+
+@main
+struct DataSimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
