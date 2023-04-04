815a5814f3648d81569f94b4edffccf25392b0ea
Carson R <carsonaurum1@icloud.com>
2023-04-04 16:19:57 -0500

    spacing update

--- DataSim/Views/Datapath/ALUView.swift
+++ DataSim/Views/Datapath/ALUView.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 
+/// An ALU shape with two output wires. This shape can be altered between a view with untappable lines, or a
+/// view with a tappable circles.
 struct ALUView: View {
     private let hasLines: Bool
     private let color: Color
@@ -28,7 +30,10 @@ struct ALUView: View {
     }
 }
 
-// Should be square
+/// An ALU shape where the body of the logic unit is not tappable and each of the four circles is selectable.
+/// - Description: This view is important to provide the functionality for individual wires to be configured
+///   within the program.
+/// - Note: Should be square
 struct ALUCircleView: View {
     
     private let color: Color
@@ -44,32 +49,42 @@ struct ALUCircleView: View {
                 ALUShape()
                     .fill(self.color)
                     .allowsHitTesting(false)
-                    .frame(width: proxy.size.width / 2, height: proxy.size.height)
-                    .position(x: proxy.size.width / 2, y: proxy.size.height / 2)
+                    .frame(width: proxy.size.width / 2,
+                           height: proxy.size.height)
+                    .position(x: proxy.size.width / 2,
+                              y: proxy.size.height / 2)
                 // Top left
                 Circle()
                     .fill(self.secondaryColor)
                     .allowsHitTesting(true)
-                    .frame(width: proxy.size.width / 4, height: proxy.size.height / 4)
-                    .position(x: proxy.size.width / 4, y: proxy.size.height / 5)
+                    .frame(width: proxy.size.width / 4,
+                           height: proxy.size.height / 4)
+                    .position(x: proxy.size.width / 4,
+                              y: proxy.size.height / 5)
                 // Bottom left
                 Circle()
                     .fill(self.secondaryColor)
                     .allowsHitTesting(true)
-                    .frame(width: proxy.size.width / 4, height: proxy.size.height / 4)
-                    .position(x: proxy.size.width / 4, y: proxy.size.height - proxy.size.height / 5)
+                    .frame(width: proxy.size.width / 4,
+                           height: proxy.size.height / 4)
+                    .position(x: proxy.size.width / 4,
+                              y: proxy.size.height - proxy.size.height / 5)
                 // Top right
                 Circle()
                     .fill(self.secondaryColor)
                     .allowsHitTesting(true)
-                    .frame(width: proxy.size.width / 4, height: proxy.size.height / 4)
-                    .position(x: proxy.size.width - (proxy.size.width / 4), y: (proxy.size.height / 8) * 3)
+                    .frame(width: proxy.size.width / 4,
+                           height: proxy.size.height / 4)
+                    .position(x: proxy.size.width - (proxy.size.width / 4),
+                              y: (proxy.size.height / 8) * 3)
                 // Bottom right
                 Circle()
                     .fill(self.secondaryColor)
                     .allowsHitTesting(true)
-                    .frame(width: proxy.size.width / 4, height: proxy.size.height / 4)
-                    .position(x: proxy.size.width - (proxy.size.width / 4), y: (proxy.size.height / 8) * 5)
+                    .frame(width: proxy.size.width / 4,
+                           height: proxy.size.height / 4)
+                    .position(x: proxy.size.width - (proxy.size.width / 4),
+                              y: (proxy.size.height / 8) * 5)
             }
         }
     }
@@ -90,30 +105,40 @@ struct ALULineView: View {
                 Rectangle()
                     .fill(self.color)
                     .allowsHitTesting(false)
-                    .frame(width: proxy.size.width / 4, height: proxy.size.height / 40)
-                    .position(x: proxy.size.width / 8, y: proxy.size.height / 5)
+                    .frame(width: proxy.size.width / 4,
+                           height: proxy.size.height / 40)
+                    .position(x: proxy.size.width / 8,
+                              y: proxy.size.height / 5)
                 // Bottom left
                 Rectangle()
                     .fill(self.color)
                     .allowsHitTesting(false)
-                    .frame(width: proxy.size.width / 4, height: proxy.size.height / 40)
-                    .position(x: proxy.size.width / 8, y: proxy.size.height - proxy.size.height / 5)
+                    .frame(width: proxy.size.width / 4,
+                           height: proxy.size.height / 40)
+                    .position(x: proxy.size.width / 8,
+                              y: proxy.size.height - proxy.size.height / 5)
                 // Top right
                 Rectangle()
                     .fill(self.color)
                     .allowsHitTesting(false)
-                    .frame(width: proxy.size.width / 4, height: proxy.size.height / 40)
-                    .position(x: proxy.size.width - (proxy.size.width / 8), y: (proxy.size.height / 8) * 3)
+                    .frame(width: proxy.size.width / 4,
+                           height: proxy.size.height / 40)
+                    .position(x: proxy.size.width - (proxy.size.width / 8),
+                              y: (proxy.size.height / 8) * 3)
                 // Bottom right
                 Rectangle()
                     .fill(self.color)
                     .allowsHitTesting(false)
-                    .frame(width: proxy.size.width / 4, height: proxy.size.height / 40)
-                    .position(x: proxy.size.width - (proxy.size.width / 8), y: (proxy.size.height / 8) * 5)
+                    .frame(width: proxy.size.width / 4,
+                           height: proxy.size.height / 40)
+                    .position(x: proxy.size.width - (proxy.size.width / 8),
+                              y: (proxy.size.height / 8) * 5)
                 ALUShape()
                     .fill(self.color)
-                    .frame(width: proxy.size.width / 2, height: proxy.size.height)
-                    .position(x: proxy.size.width / 2, y: proxy.size.height / 2)
+                    .frame(width: proxy.size.width / 2,
+                           height: proxy.size.height)
+                    .position(x: proxy.size.width / 2,
+                              y: proxy.size.height / 2)
             }
         }
     }
@@ -123,8 +148,7 @@ struct ALUView_Previews: PreviewProvider {
     static var previews: some View {
         ALUView(false,
                 color: .init(.lightGray),
-                secondaryColor: .init(.gray)
-        )
+                secondaryColor: .init(.gray))
             .frame(width: 200, height: 200)
 //            .border(.blue, width: 5)
     }
